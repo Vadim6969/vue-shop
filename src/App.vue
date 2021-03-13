ca5f3766551906dc8350aedfc9fe85fc36fc68d6
vadik <kioshima@gmail.com>
2021-03-13 17:46:16 +0300

    default prodject

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <div class="container max-w-5xl m-auto">
+      <nav class="navbar">
+        <div class="logo">VUE</div>
+        <ul class="navbar__list">
+          <li class="navbar__item"><a href="#"></a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <router-view></router-view>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
